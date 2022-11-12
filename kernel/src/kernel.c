312e12cd09708369ae7837e96ccfcbba11be9db3
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-11-12 14:39:48 -0500

    Added printf

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,15 +1,12 @@
 #include <stdint.h>
 #include <stddef.h>
+
+#include "console.h"
 #include "limine.h"
 
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent.
-
-static volatile struct limine_terminal_request terminal_request = {
-    .id = LIMINE_TERMINAL_REQUEST,
-    .revision = 0
-};
 
 static void done(void) {
     for (;;) {
@@ -19,16 +16,7 @@ static void done(void) {
 
 // The following will be our kernel's entry point.
 void _start(void) {
-    // Ensure we got a terminal
-    if (terminal_request.response == NULL
-     || terminal_request.response->terminal_count < 1) {
-        done();
-    }
-
-    // We should now be able to call the Limine terminal to print out
-    // a simple "Hello World" to screen.
-    struct limine_terminal *terminal = terminal_request.response->terminals[0];
-    terminal_request.response->write(terminal, "hewo uwu", 8);
+    console_printf("hewo\nmagic number is %x\n", 0x69);
 
     // We're done, just hang...
     done();
